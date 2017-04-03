d7744eaefbabee4b3ba4be649ae64f948e8b42ea
Bridger <bridgerrhammond@gmail.com>
2017-04-03 18:53:39 -0400

    Search box done. Haven't coded JS yet.

--- js/main.js
+++ js/main.js
@@ -10,18 +10,21 @@ I had to repeat a lot of the same code in my index.html
 // $images = glob($dirname."*.jpg");
 
 // foreach($images as $image) {
-//     echo '<a class="gallery-thumbnails" href="images/photos/01.jpg" data-lightbox="gallery" data-title="Hay Bales <br />I love hay bales. Took this snap on a drive through the countryside past some straw fields."><img class="example-image" src="'.$image.'"></a>';
+//     echo '<a class="gallery-thumbnails" href="images/photos/01.jpg" data-lightbox="gallery" data-title="Hay Bales <br />I love hay bales. Took this snap on a drive through the countryside past some straw fields."><img class="gallery-thumbnails" src="'.$image.'"></a>';
 // };
 
 /* All Images and Text */
-var imagesAndText = [
-    {imageOne: "images/photos/01.jpg",
-    imageOneThumbnail: "images/photos/thumbnails/01.jpg",
-    imageOneText: "Hay Bales <br />I love hay bales. Took this snap on a drive through the countryside past some straw fields."},
-    {imageTwo: "images/photos/02.jpg",
-    imageTwoThumbnail: "images/photos/thumbnails/02.jpg",
-    imageTwoText: "Lake <br />The lake was so calm today. We had a great view of the snow on the mountains from here."}
-];
+// var imagesAndText = [
+//     {imageOne: "images/photos/01.jpg",
+//     imageOneThumbnail: "images/photos/thumbnails/01.jpg",
+//     imageOneText: "Hay Bales <br />I love hay bales. Took this snap on a drive through the countryside past some straw fields."},
+//     {imageTwo: "images/photos/02.jpg",
+//     imageTwoThumbnail: "images/photos/thumbnails/02.jpg",
+//     imageTwoText: "Lake <br />The lake was so calm today. We had a great view of the snow on the mountains from here."}
+// ];
+
+/* Search functionality */
+var searchInput = $('.gallery-image a:[data-title==' + $(this).val() + ']');
 
 /* Lightbox */
 lightbox.option({
